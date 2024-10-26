1cdb7d5bc19762d138d8813813065d6613c22d3b
Mateus Zanario <Mateusmartineztwo@gmail.com>
2024-10-26 19:59:20 -0400

    sexto commit

--- 01 - Tópicos/13 - Arrays/01 - Array vetor/main.c
+++ 01 - Tópicos/13 - Arrays/01 - Array vetor/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 5
 
 int main()
 {
@@ -18,17 +19,43 @@ int main()
   Alguns exemplos:
   int n = 5;
   int vetor[n]   -> Errado
+  float F[N + 1] -> Correto
   char texto[30] -> Correto
   int v[4.5]     -> Errado
 
-  Índice:
   Para acessar os elementos de um array,
   usamos o que chamamamos de índice:
-  float notas[100]; -> Vai da posição 0 até a 99
-  notas[0] = 81;  -> Primeiro índice
-  notas[1] = 55;  -> Segundo índice
-  notas[99] = 72; -> Último índice
+  float notas[100]; -> De 0 até a 99
+  notas[0] = 81;    -> Primeiro índice
+  notas[1] = 55;    -> Segundo índice
+  notas[99] = 72;   -> Último índice
+
+  Cada posição de um array é uma variável:
+  float notas[100], vetor[100];
+  scanf("%d", &notas[5]);         -> leitura
+  notas[0] = 10;                  -> atribuição
+  notas[1] = notas[5] + notas[0]; -> expressão
+  notas = vetor                   -> errado
   */
+
+  // exemplo:
+  float notas[N], media = 0.0;
+  int i;
+  for (i = 0; i < N; i++)
+  {
+    printf("Digite a nota do aluno %d: ", i);
+    scanf("%f", &notas[i]);
+    media = media + notas[i];
+  }
+
+  media = media / N;
+  for (i = 0; i < N; i++)
+  {
+    if (notas[i] > media)
+    {
+      printf("Aluno %d: %.2f\n", i, notas[i]);
+    }
+  }
 
   return 0;
 }
