139dc83aa56c1abf5b70902a6de4152b8d9bbaee
Mateus Zanario <Mateusmartineztwo@gmail.com>
2024-10-27 21:07:56 -0400

    oitavo commit

---
+++ 01 - Tópicos/13 - Arrays/02 - Array matriz/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 100
+
+int main()
+{
+  /*
+  Definição:
+  Uma array bidimensional(ou matriz) é um tipo
+  de array que possui duas dimensões, representadas
+  por linhas e colunas.
+
+  Forma geral:
+  tipo nome_array[num_linhas][num_colunas];
+
+  Alguns exemplos:
+  int n = 5;
+  int matriz[3][2];    -> Correto
+  float F[N + 1][N];   -> Correto
+  char frases[30][50]; -> Correto
+  int mat[n][n];       -> Errado
+  int M[4.5][5];       -> Errado
+
+  Acesso aos elementos pelo índice:
+  int mat[3][2];
+  mat[0][0] = 5; -> Primeira posição
+  mat[2][1] = 3; -> Última posição
+  */
+
+  return 0;
+}
