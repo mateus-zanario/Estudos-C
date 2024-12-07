5a9b3afdf167ebff5cdf27f21874e4b899ccd2fa
Mateus Zanario <Mateusmartineztwo@gmail.com>
2024-12-07 19:14:27 -0400

    décimo sex. coomit

--- 01 - Tópicos/14 - Strings/03 - Inversão de strings/main.c
+++ 01 - Tópicos/14 - Strings/03 - Inversão de strings/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+  // Inversão de strings
+  // Exemplo 1
+  char palavra1[120] = "Linguagem C";
+  int i;
+  for (i = strlen(palavra1) - 1; i >= 0; i--)
+  {
+    printf("%c", palavra1[i]);
+  }
+  printf("\n");
+  printf("------------------\n");
+
+  // Exemplo 2
+  char palavra2[20] = "Linguagem C";
+  char palavra3[20];
+  int x, y = 0;
+  for (x = strlen(palavra2) - 1; x >= 0; x--)
+  {
+    palavra3[y] = palavra2[x];
+    y++;
+  }
+  palavra3[y] = '\0';
+  printf("palavra3 = %s\n", palavra3);
 
   return 0;
 }
--- 01 - Tópicos/14 - Strings/04 - Contagem de caracteres/main.c
+++ 01 - Tópicos/14 - Strings/04 - Contagem de caracteres/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+  /*
+  E se desejássemos, por exemplo, contar quantas
+  vogais estão presentes em uma string, como
+  faríamos para descobrir isso? Segue alguns exemplos.
+  */
+
+  // Exemplo 1:
+  char string1[20] = "Linguagem C";
+  int i, total = 0;
+  int tam1 = strlen(string1);
+  for (i = 0; i < tam1; i++)
+  {
+    if (string1[i] == 'a' || string1[i] == 'e' || string1[i] == 'i' || string1[i] == 'o' || string1[i] == 'u')
+    {
+      total++;
+    }
+  }
+  printf("Número de vogais: %d\n", total);
+  printf("---------------------\n");
+
+  // Exemplo 2:
+  char string2[20] = "Linguagem C";
+  char vogais[11] = "aeiouAEIOU";
+  int x, y, total_vogais = 0;
+  int tam2 = strlen(string2);
+  int tam3 = strlen(vogais);
+  for (x = 0; x < tam2; x++)
+  {
+    for (y = 0; y < tam3; y++)
+    {
+      if (string2[x] == vogais[y])
+      {
+        total_vogais++;
+        break;
+      }
+    }
+  }
+  printf("Número de vogais: %d\n", total_vogais);
 
   return 0;
 }
